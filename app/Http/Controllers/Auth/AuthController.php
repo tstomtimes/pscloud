35185668c1ed8cb0016b955dd0e49bdfadfcde84
Tsutomu Morishita <tom@studiom.tokyo>
2019-05-24 22:41:07 +0900

    test

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -30,9 +30,9 @@ class AuthController extends \App\Http\Controllers\Controller
         try{
             $userSocial = Socialize::with('graph')->user();
             $user = User::where(['email' => $userSocial->getEmail()])->first();
-            dump("I'm back!");
+            var_dump("I'm back!");
             if(empty($user)){
-              dump("We have not such a name here! Let's make that!");
+              var_dump("We have not such a name here! Let's make that!");
                 $newuser = new User;
                 $newuser->name = $userSocial->getName();
                 $newuser->email = $userSocial->getEmail();
@@ -43,9 +43,9 @@ class AuthController extends \App\Http\Controllers\Controller
                     route('verification.resend');
                 }
             }
-            dump("Login check!");
+            var_dump("Login check!");
             Auth::login($user);
-            dump("Let's move to Dashboad");
+            var_dump("Let's move to Dashboad");
             return redirect("/dashboard");
         } catch(\Exception $e) {
             return redirect("/");
